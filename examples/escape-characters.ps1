b3e072be5468957739b93e37df9a1881a8c1c4e5
Kevin Kane <kkane@ncartmuseum.org>
2023-11-02 11:43:43 -0400

    add hashtables System.Object example and special escape characters example

---
+++ examples/escape-characters.ps1
@@ -0,0 +1,28 @@
+﻿# About special escape characters in PowerShell
+#
+# See
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_special_characters?view=powershell-5.1
+
+function Blank-Line {
+    Write-Host "`n"
+}
+
+function Carriage-Return {
+    Write-Host "`r"
+}
+
+function Horizontal-Tab {
+    return "`t"
+}
+
+
+$something = "something"
+$somethingElse = "something else"
+$somethingMore = "something more"
+
+Write-Host $something
+Blank-Line
+Write-Host $somethingElse
+Carriage-Return
+Write-Host "$something and $somethingElse"
+Write-Host (Horizontal-Tab) "and $somethingMore and $somethingElse" # parens allows Horizontal-Tab func to execute first, and then concatenate returned string
---
+++ examples/hashtables-system-object.ps1
@@ -0,0 +1,11 @@
+﻿$object = New-Object System.Object
+$object | Add-Member -type NoteProperty -name Hostname -Value "Hello World!"
+$object | Add-Member -type NoteProperty -name IP -Value "192.168.1.10"
+
+Write-Host ($object | Format-Table | Out-String)
+
+Write-Host ($object | Format-List | Out-String)
+
+Write-Host $object.Hostname
+
+Write-Host $object.IP
