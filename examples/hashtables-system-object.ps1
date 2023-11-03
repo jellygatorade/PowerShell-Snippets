3b94f830f61eb7ef117e008ee2803af8eacb24bc
Kevin Kane <kkane@ncartmuseum.org>
2023-11-03 08:48:08 -0400

    add note

--- examples/hashtables-system-object.ps1
+++ examples/hashtables-system-object.ps1
@@ -2,6 +2,8 @@
 $object | Add-Member -type NoteProperty -name Hostname -Value "Hello World!"
 $object | Add-Member -type NoteProperty -name IP -Value "192.168.1.10"
 
+Write-Host $object.GetType() # System.Object
+
 Write-Host ($object | Format-Table | Out-String)
 
 Write-Host ($object | Format-List | Out-String)
