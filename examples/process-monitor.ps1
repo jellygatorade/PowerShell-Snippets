d971da98ef1fc4fdc5896fce5eaaa8497925f700
Kevin Kane <kkane@ncartmuseum.org>
2023-11-07 15:17:07 -0500

    add process-monitor example

---
+++ examples/process-monitor.ps1
@@ -0,0 +1,11 @@
+﻿# Something like ps/top in Linux
+
+# Original code
+# while (1) { ps | sort -desc cpu | select -first 30; sleep -seconds 2; cls }
+
+# Reformatted in PowerShell language conventions
+while ($true) { 
+    Get-Process | Sort-Object -desc cpu | Select-Object -first 30
+    Start-Sleep -seconds 2
+    Clear-Host
+}
