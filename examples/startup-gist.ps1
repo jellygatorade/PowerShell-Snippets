3b15462dc0ab6f3ee673e5a10bf510605c81ea17
Kevin Kane <kkane@ncartmuseum.org>
2024-02-02 09:04:36 -0500

    add startup-gist example

---
+++ examples/startup-gist.ps1
@@ -0,0 +1,27 @@
+﻿# -----------------------------------------------------------------
+# Copied notes, not currently tested 2/2/2024 ---------------------
+# -----------------------------------------------------------------
+
+# $WeWant = "executable"
+# $WeWant = "mpc"
+$WeWant = "vlc"
+
+Switch ($WeWant)
+{
+    "executable" 
+    {
+	    cd C:\Users\Username\Desktop\Path\To\
+	    .\executable.exe
+
+        # or
+        # & "C:\Users\Username\Desktop\Path\To\executable.exe"
+    }
+    "mpc" 
+    {
+        & "C:\Path\To\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe" "/fullscreen" "C:\Users\Username\Desktop\video-filename.mp4"
+    }
+    "vlc" 
+    {
+        & "C:\Path\To\VideoLAN\VLC\vlc.exe"  "--video-on-top --no-video-deco --fullscreen --no-autoscale --loop --no-video-title" "C:\Users\Username\Desktop\video-filename.mp4" 
+    }
+}
