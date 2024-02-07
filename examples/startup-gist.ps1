2d1c0ff38c8f02ad97250f7d723fe686d29ed927
Kevin Kane <kkane@ncartmuseum.org>
2024-02-07 10:09:07 -0500

    update startup gist syntax

--- examples/startup-gist.ps1
+++ examples/startup-gist.ps1
@@ -1,27 +1,35 @@
-﻿# -----------------------------------------------------------------
-# Copied notes, not currently tested 2/2/2024 ---------------------
+﻿# Config  ---------------------------------------------------------
+# Must specify exactly one filepath and one program to run! -------
 # -----------------------------------------------------------------
 
-# $WeWant = "executable"
-# $WeWant = "mpc"
-$WeWant = "vlc"
+# $FilePath = 'C:\Users\Username\Desktop\Path\To\executable.exe'
+$FilePath = 'C:\Users\kkane.MOA\OneDrive - The North Carolina Museum of Art Foundation, Inc\Desktop\Programming\NCMA Projects\Video Samples\20220617-KK Sample-Amp Concert-01-1080p.mp4' 
 
-Switch ($WeWant)
+# $ProgramToRun = 'executable'
+# $ProgramToRun = 'mpc'
+$ProgramToRun = 'vlc'
+
+
+
+# -----------------------------------------------------------------
+# Do not edit below here ------------------------------------------
+# -----------------------------------------------------------------
+
+$mpc_exe_path = 'C:\Path\To\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe'
+$vlc_exe_path = 'C:\Program Files (x86)\VideoLAN\VLC\vlc.exe'
+
+Switch ($ProgramToRun)
 {
     "executable" 
     {
-	    cd C:\Users\Username\Desktop\Path\To\
-	    .\executable.exe
-
-        # or
-        # & "C:\Users\Username\Desktop\Path\To\executable.exe"
+        & $FilePath
     }
     "mpc" 
     {
-        & "C:\Path\To\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe" "/fullscreen" "C:\Users\Username\Desktop\video-filename.mp4"
+        & $mpc_exe_path /fullscreen $FilePath
     }
     "vlc" 
-    {
-        & "C:\Path\To\VideoLAN\VLC\vlc.exe"  "--video-on-top --no-video-deco --fullscreen --no-autoscale --loop --no-video-title" "C:\Users\Username\Desktop\video-filename.mp4" 
+    {  
+        & $vlc_exe_path --video-on-top --no-video-deco --fullscreen --no-autoscale --loop --no-video-title $FilePath
     }
 }
