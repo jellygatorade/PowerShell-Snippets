83e40eef99685696ca76d6d6094a6a259a033c17
Kevin Kane <kkane@ncartmuseum.org>
2024-03-28 15:52:30 -0400

    edit example directory path

--- examples/detect-multi-display.ps1
+++ examples/detect-multi-display.ps1
@@ -7,9 +7,5 @@ while ($totalDisplays -lt 2) {
     Start-Sleep -seconds 5
 }
 
-cd "C:\Users\kkane.MOA\Non-OneDrive Docs\Unity Projects\Dutch Art in a Global Age\Builds"
+cd "C:\Users\Username\Desktop\whereever"
 Start-Process -FilePath ".\Dutch Art in a Global Age.exe"
-
-# for writing the output to a log file
-# try powershell -windowstyle minimized .\myscript.ps1 -verbose >> \desktop\ps.log
-# https://stackoverflow.com/questions/13784382/how-can-i-redirect-powershell-output-when-run-from-task-scheduler
