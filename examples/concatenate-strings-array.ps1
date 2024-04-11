5d0aac57f3307156e460ecc426e09efeefa7b7e2
Kevin Kane <kkane@ncartmuseum.org>
2024-04-11 10:33:48 -0400

    complete delete-within-subdirectory and add example concatenate-strings-array

---
+++ examples/concatenate-strings-array.ps1
@@ -0,0 +1,12 @@
+﻿## Create a new array with * prepended to each string in the original array
+
+$extFilters         = '.jpg' , '.png', '.tif'
+$extFiltersWildcard = @()
+
+$extFilters | % { 
+    $item = -join("*", $_)
+    $extFiltersWildcard += $item
+}
+
+Write-Host $extFilters
+Write-Host $extFiltersWildcard
--- examples/delete-within-subdirectory.ps1
+++ examples/delete-within-subdirectory.ps1
@@ -1,33 +1,105 @@
-$parentDirPath    = "C:\Users\kkane.MOA\Non-OneDrive\PowerShell\Test Directory\*\Trash"
-$filters1         = '.txt'
-$filters2         = '.jpg' , '.png', '.tif'
-$filtersWildcard  = '*.jpg' , '*.png', '*.tif'
+# ---------------------------------------------------------------------
+# 
+# This script deletes files 
+# with specified extensions            ($extFilters)
+# within a specified parent directory  ($parentDirPath)
+# 
+#
+# Using these default aliases
+#
+# Where == Where-Object
+# %     == ForEach-Object
+#
+# ---------------------------------------------------------------------
 
-# Where is alias for Where-Object
-# % is alias for ForEach-Object
+# Setup - Variables ---------------------------------------------------
 
-# Find all paths to delete
-# Get-ChildItem Method
+$parentDirPath      = "C:\Users\kkane.MOA\Non-OneDrive\PowerShell\Test Directory\*\Trash"
+$extFilters         = '.jpg' , '.png', '.tif'
+#$extFiltersWildcard = @()
+$deletePaths        = @()
 
-#Get-ChildItem $parentDirPath -Recurse | Where {$filters1 -contains $_.extension} | % { Write-Host $_.FullName }
+# Setup - Functions ---------------------------------------------------
 
-# Find all paths to delete
-# Resolve-Path Method
-# Faster than Get-ChildItem because doesn't read all the extra data that Get-ChildItem does
+function InitInstructions {
+    # User Instructions
 
-$filtersWildcard | % { Join-Path $parentDirPath $_ } | Resolve-Path | % { Write-Host $_ }
-
-# # Leaves the trash folder
-# Remove-Item -Path ".\Test Directory\*\Trash\*.*" -Force
-
-# # Deletes the trash folder
-# Remove-Item -Path ".\Test Directory\*\Trash\" -Recurse -Force
-
-
-$userResponse = Read-Host "Proceed? Type 'yes' to continue"
-
-switch ($userResponse)
-{
-    "yes" { Write-Host "Continuing" }
-    default { Write-Host "Exiting" } 
+    Write-Host "This script will find all items in Capture One ...\<Project Name>\Trash directors for deletion."
+    Write-Host "The file extensions to be deleted include:"
+    Write-Host "" # Print blank line
+    $filtersWildcard | % { Write-Host $_ }
+    Write-Host "" # Print blank line
+    $userResponse = Read-Host "Proceed? Type 'ok' to continue"
+    
+    switch ($userResponse)
+    {
+        "ok" { LaunchFindDeleteProcess }
+        default { ExitScript } 
+    }
 }
+
+function FindPaths {
+
+    # Find all paths to delete
+
+    # Get-ChildItem Method
+    #Get-ChildItem $parentDirPath -Recurse | Where {$extFilters -contains $_.extension} | % { $deletePaths += $_.FullName }
+
+    # Resolve-Path Method
+    # Faster than Get-ChildItem because doesn't read all the extra data that Get-ChildItem does
+    
+    # Need to prepend a wildcard on each extension ('.jpg' , '.png', '.tif') -> ('*.jpg' , '*.png', '*.tif')
+    $extFilters | % { 
+        $item = -join("*", $_)
+        $extFiltersWildcard += $item
+    }
+
+    $extFiltersWildcard | % { Join-Path $parentDirPath $_ } | Resolve-Path | % { $deletePaths += $_ }
+    
+    $deletePaths # return this (this is how PowerShell function returns work)
+}
+
+function DeletePaths {
+    try {
+        $deletePaths | % { Remove-Item -Path $_ -Force }
+        Write-Host "All files deleted successfully."
+    }
+    catch {
+        Write-Host "An error occurred. Is any file locked for editing?"
+    }
+}
+
+function ExitScript {
+    Write-Host "Exiting"
+    Exit
+}
+
+function LaunchFindDeleteProcess {
+    Write-Host "" # Print blank line
+
+    $deletePaths = FindPaths # Find paths for deletion
+    $deletePaths | % { Write-Host $_ } # Print paths for deletion
+    $totalToDelete = $deletePaths.length
+
+    Write-Host "" # Print blank line
+
+    if ($totalToDelete -eq 0) { 
+        Write-Host "No files were found to delete. Exiting."
+        return 
+    }
+    
+    # User confirmation
+    
+    $userResponse = Read-Host "Proceed with deleting $totalToDelete files? Type 'permanently delete' to continue"
+    
+    switch ($userResponse)
+    {
+        "permanently delete" { DeletePaths }
+        default { ExitScript } 
+    }
+
+}
+
+# Init / Execute ------------------------------------------------------
+
+InitInstructions
