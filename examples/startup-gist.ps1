69157b1b77a473d0f501468cdd6f440e23b88ead
Kevin Kane <kkane@ncartmuseum.org>
2024-10-02 15:36:51 -0400

    add Chrome and Edge for displaying static image or webpage in fullscreen at startup

--- examples/startup-gist.ps1
+++ examples/startup-gist.ps1
@@ -3,21 +3,27 @@
 # -----------------------------------------------------------------
 
 # $FilePath = 'C:\Users\Username\Desktop\Path\To\executable.exe'
-$FilePath = 'C:\Users\Username\Desktop\Path\To\video.mp4' 
+# $FilePath = 'C:\Users\Username\Desktop\Path\To\video.mp4' 
+$FilePath = 'file:///C:/Users/kkane.MOA/Downloads/mockup-still-009.png'
 
 # $ProgramToRun = 'executable'
 # $ProgramToRun = 'mpc'
-$ProgramToRun = 'vlc'
-
+# $ProgramToRun = 'vlc'
+$ProgramToRun = 'chrome'
+# $ProgramToRun = 'edge'
 
 
 # -----------------------------------------------------------------
-# Do not edit below here ------------------------------------------
+# Do not edit below - unless you are familiar with PowerShell -----
 # -----------------------------------------------------------------
 
 $mpc_exe_path = 'C:\Path\To\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe'
 # $vlc_exe_path = 'C:\Program Files\VideoLAN\VLC\vlc.exe'
 $vlc_exe_path = 'C:\Program Files (x86)\VideoLAN\VLC\vlc.exe'
+$chrome_exe_path = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
+$edge_exe_path = 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
+
+Write-Host $ProgramToRun $
 
 Switch ($ProgramToRun)
 {
@@ -32,6 +38,15 @@ Switch ($ProgramToRun)
     "vlc" 
     {  
         & $vlc_exe_path --video-on-top --no-video-deco --fullscreen --no-autoscale --loop --no-video-title $FilePath
+    }
+    "chrome" 
+    {  
+        & $chrome_exe_path --start-fullscreen $FilePath
+    }
+    "edge" 
+    {  
+        # For msedge.exe --start-fullscreen switch to work, open Edge -> 3 dots (hamburger menu) -> Settings -> System and Performance -> Disable 'Startup boos't & Disable 'Continue running background extensions and apps when Microsoft Edge is closed'
+        & $edge_exe_path --start-fullscreen $FilePath
     }
     default
     {
